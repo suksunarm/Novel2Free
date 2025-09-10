f75e7cfb019f76671c5a8e15d05fd25b5f366382
Kubnong <riw3566@gmail.com>
2025-09-10 15:06:54 +0700

    [Add] setting EJS

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,28 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 
+// router.get("/add_novel", (req,res) => {
+//     res.sendFile(path.join(__dirname,"..","views","add_novel.html"));
+// })
+
+// router.get("/add_redeem", (req,res) => {
+//     res.sendFile(path.join(__dirname,"..","views","add_redeem_code.html"));
+// })
+
+// router.get("/dashboard", (req,res) => {
+//     res.sendFile(path.join(__dirname,"..","views","dashboard_admin.html"));
+// })
+
 router.get("/add_novel", (req,res) => {
-    res.sendFile(path.join(__dirname,"..","views","add_novel.html"));
+    res.render('add_novel', { pageTitle: 'เพิ่มนิยาย'});
 })
 
 router.get("/add_redeem", (req,res) => {
-    res.sendFile(path.join(__dirname,"..","views","add_redeem_code.html"));
+    res.render('add_redeem_code', { pageTitle: 'เพิ่มคูปอง'});
 })
 
 router.get("/dashboard", (req,res) => {
-    res.sendFile(path.join(__dirname,"..","views","dashboard_admin.html"));
+    res.render('dashboard_admin', { pageTitle: 'แดชบอร์ด'});
 })
 
 module.exports = router
